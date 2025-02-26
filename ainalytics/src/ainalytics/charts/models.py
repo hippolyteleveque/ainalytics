834a9ad9194db62424d99711f83192e39ec0de18
Hippolyte Lévêque <hippolyte.leveque@essec.edu>
2025-02-26 21:07:53 +0100

    Updated ainalytics

--- ainalytics/src/ainalytics/charts/models.py
+++ ainalytics/src/ainalytics/charts/models.py
@@ -1,8 +1,11 @@
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
 
-from sqlmodel import Field
+from sqlmodel import Field, Relationship
 
 from ainalytics.database import AppTable
+
+if TYPE_CHECKING:
+    from ainalytics.auth.models import User
 
 
 class PersistedChart(AppTable, table=True):
@@ -11,3 +14,6 @@ class PersistedChart(AppTable, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     type: str
     query: str
+    user_id: int = Field(foreign_key="users.id")
+
+    user: "User" = Relationship(back_populates="charts")
