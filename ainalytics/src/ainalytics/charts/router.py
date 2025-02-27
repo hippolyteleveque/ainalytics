a1e2ef62b6d8202ab289a0897c5e9abfc6c477f8
Hippolyte Lévêque <hippolyte.leveque@essec.edu>
2025-02-27 13:34:18 +0100

    Added handling of users with charts

--- ainalytics/src/ainalytics/charts/router.py
+++ ainalytics/src/ainalytics/charts/router.py
@@ -1,24 +1,25 @@
 from fastapi import APIRouter
 
-from .service import get_charts, create_chart, chart_delete
+from ainalytics.auth.service import CurrentUser
+from .service import get_charts, create_chart, delete_chart_user
 from .schemas import Charts, ChartIn, Chart
 
 router = APIRouter(prefix="/charts", tags=["charts"])
 
 
 @router.get("/", response_model=Charts)
-def charts():
-    objs = get_charts()
+def charts(current_user: CurrentUser):
+    objs = get_charts(current_user.id)
     return Charts(charts=objs)
 
 
-@router.post("/")
-def post_chart(request: ChartIn):
-    obj = create_chart(request.query, request.type)
+@router.post("/", response_model=Chart)
+def post_chart(request: ChartIn, current_user: CurrentUser):
+    obj = create_chart(request.query, request.type, current_user.id)
     return Chart(**obj.model_dump())
 
 
 @router.delete("/{id}")
-def delete_chart(id: int):
-    obj = chart_delete(id)
+def delete_chart(id: int, current_user: CurrentUser):
+    obj = delete_chart_user(id, current_user.id)
     return obj
